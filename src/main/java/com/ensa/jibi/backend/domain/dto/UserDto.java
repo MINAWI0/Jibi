31ac6fefb1c0216f9bc7744ad5ecce3df81f1209
Mehdi <mehdibaallal@gmail.com>
2024-05-23 17:16:29 +0100

    added some components for backend

---
+++ src/main/java/com/ensa/jibi/backend/domain/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.ensa.jibi.backend.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto{
+    private String nom;
+    private String prenom;
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/ensa/jibi/backend/repositories/AgentRepository.java
@@ -0,0 +1,10 @@
+package com.ensa.jibi.backend.repositories;
+
+import com.ensa.jibi.backend.domain.entities.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgentRepository extends JpaRepository<Agent, Long> {
+
+}
