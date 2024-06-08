22fb3f5f5f9ebcb6945b2c1e8586c3e813001305
chaymaeben4 <chaymaebenlagraoui47@gmail.com>
2024-06-08 15:16:11 +0100

    commit

--- src/main/java/com/ensa/jibi/domain/enums/RechargeAmmount.java
+++ src/main/java/com/ensa/jibi/domain/enums/RechargeAmmount.java
@@ -1,13 +1,13 @@
 package com.ensa.jibi.domain.enums;
 
 public enum RechargeAmmount {
-    RECHARGE_AMMOUNT_10(10),
-    RECHARGE_AMMOUNT_20(20),
-    RECHARGE_AMMOUNT_50(50),
-    RECHARGE_AMMOUNT_70(70),
-    RECHARGE_AMMOUNT_80(80),
-    RECHARGE_AMMOUNT_90(90),
-    RECHARGE_AMMOUNT_100(100);
+    RECHARGE_10DH(10),
+    RECHARGE_20DH(20),
+    RECHARGE_50DH(50),
+    RECHARGE_70DH(70),
+    RECHARGE_80DH(80),
+    RECHARGE_90DH(90),
+    RECHARGE_100DH(100);
 
     private final int value;
 
