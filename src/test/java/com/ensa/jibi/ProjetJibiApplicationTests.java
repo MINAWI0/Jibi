23a1fa215a575b08b5f2d08b2a752944f8a92b15
Youssafouhba <youssefouhba@gmail.com>
2024-06-11 10:48:54 +0100

    deploy updates

--- src/test/java/com/ensa/jibi/ProjetJibiApplicationTests.java
+++ src/test/java/com/ensa/jibi/ProjetJibiApplicationTests.java
@@ -1,13 +1,10 @@
 package com.ensa.jibi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjetJibiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
